ad81e63584b4bdc536e7289339b20ed6d9413697
quentin-HP-Ubuntu <quentin.loriaux@ensta-paris.fr>
2024-05-25 20:45:08 -0400

    fixed installer

--- installpk.sh
+++ installpk.sh
@@ -36,31 +36,3 @@ make
 # done!
 echo "PressKey successfully (hopefully) installed!"
 
-
-
-
-# Install libsfml-dev
-apt install libsfml-dev -y
-
-# Install librapidxml-dev
-apt install librapidxml-dev -y
-
-# Install clang++-16
-if ! command -v clang++-16 &>/dev/null; then
-    # If clang++-16 doesn't exist, proceed with installation
-    wget https://apt.llvm.org/llvm.sh
-    chmod +x llvm.sh
-    ./llvm.sh 16
-    rm llvm.sh
-else
-    echo "clang++-16 is already installed."
-fi
-
-# Install make
-apt install make -y
-
-# Run make in current directory
-make
-
-# Execute the resulting binary
-./Bomberman_Plat.x
